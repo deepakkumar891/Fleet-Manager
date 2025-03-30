71b63d050b0ee6d11400b0a6e50ace7b6224503e
deepakkumar891 <64356210+deepakkumar891@users.noreply.github.com>
2025-03-30 12:17:31 +0530

    Initial Commit

---
+++ Fleet ManagerTests/Fleet_ManagerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Fleet_ManagerTests.swift
+//  Fleet ManagerTests
+//
+//  Created by Deepak Kumar on 30/03/2025.
+//
+
+import Testing
+@testable import Fleet_Manager
+
+struct Fleet_ManagerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
